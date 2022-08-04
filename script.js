e606392dc162ba45b374678ed078393a1238ead4
Raheel Ahmad <raheelA0@pm.me>
2022-08-04 17:54:05 -0400

    Change minor appearance details, add reset button, add player name input fields

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const MAX_COUNTER = 9;
 
-const Player = (mark) => {
+const Player = (mark, playerName) => {
     const getMark = () => mark;
+    const getName = () => playerName;
 
-    return {getMark};
+
+    return {getMark, getName};
 };
 
 const game = (() => {
     let gameCounter = 0;
     let turnToken = 0;
-    const player1 = Player("x");
-    const player2 = Player('o');
+    const player1 = Player("x", "player1");
+    const player2 = Player('o', "player2");
 
     const setCurrentPlayer = () => {
         if (turnToken == 0){
@@ -25,6 +27,27 @@ const game = (() => {
     const getCurrentPlayer = () => {
             const currentPlayer = setCurrentPlayer();
             return currentPlayer;
+    }
+
+    const displayResult = (num) => {
+        // Get the modal
+        var modal = document.getElementById("resultModal");
+        modal.style.display = "grid";
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+            modal.style.display = "none";
+            }
+        }
     }
 
     const checkBoard = (gameArray) => {
@@ -42,9 +65,11 @@ const game = (() => {
         ];
 
         if (winArray.some(element => verifyArray(gameArray, element))) {
-            console.log("win");
+            displayResult(1);
+        }else if (gameCounter < MAX_COUNTER) {
+            gameCounter++;
         }else {
-            console.log("not yet");
+            displayResult(2);
         }
     }
 
